d2bcaf3585d5ea79ccf5a1a2e86302c66004a5bc
Isaac Pereira Ferreira <isaacjf321@gmail.com>
2026-03-06 13:49:24 -0300

    Add Exercicio de encapsulamento POO C#

---
+++ C#/Banco Simples POO/BancoSimples/BancoSimples.cs
@@ -0,0 +1,50 @@
+﻿using BancoSimples;
+using System.Globalization;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Banco
+{
+    class BancoSimples
+    {
+        static public void Main()
+        {
+            Conta c; //posso declarar a variavel aq fora e instanciar dentro 
+                
+            Console.Write("Entre o número da conta: ");
+            int numeroConta = int.Parse(Console.ReadLine());
+            Console.Write("Entre o titular da conta: ");
+            string nome = Console.ReadLine();
+            Console.Write("Haverá depósito inicial (s/n)? ");
+            char esc = char.Parse(Console.ReadLine());
+            if (esc == 's' || esc == 'S')
+            {
+                Console.Write("Entre o valor de depósito inicial: ");
+                double saldoInicial = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                c = new Conta(numeroConta, nome, saldoInicial); //instanciado caso tenha deposito 
+            }
+            else
+            {
+                c = new Conta(numeroConta, nome); //instanciando sem deposito 
+            }
+            Console.WriteLine();
+
+            //deposito
+            Console.WriteLine("Dados da conta: ");
+            Console.WriteLine(c);
+            Console.WriteLine();
+            Console.Write("Entre um valor para depósito: ");
+            double deposito = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            c.Deposito(deposito);
+            Console.WriteLine("Dados da conta atualizados: ");
+            Console.WriteLine(c);
+            Console.WriteLine();
+
+            //saque
+            Console.Write("Entre um valor para saque: ");
+            double saque = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            c.Saque(saque);
+            Console.WriteLine("Dados da conta atualizados: ");
+            Console.WriteLine(c);
+        }
+    }
+}
---
+++ C#/Banco Simples POO/BancoSimples/Conta.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BancoSimples
+{
+    internal class Conta
+    {
+        //encapsulando os dados 
+        public int NumeroConta { get; private set; } //o numero da conta não pode ser mudado, melhor aplicar pelo constructor 
+        public string Nome { get; set; }
+        public double Saldo { get; private set; }
+
+        //construtores 
+        public Conta(int conta, string nome, double saldoInicial)
+        {
+            NumeroConta = conta; //construtor tem permissao para acessar o private set 
+            Nome = nome;
+            Deposito(saldoInicial); //metodo que faz entrada de saldo na conta 
+        }
+
+        public Conta(int conta, string nome): this(conta,nome,0.0) //sobrecarga
+        {
+            //saldo fica zerado nessa situação
+        }
+
+
+        //metodos
+        public void Deposito(double entrada)
+        {
+            Saldo += entrada;
+        }
+
+        public void Saque(double saida)
+        {
+            Saldo -= saida;
+            Saldo -= 5; //taxa de saque
+        }
+
+        public override string ToString()
+        {
+            return $"Conta {NumeroConta}, Titular: {Nome}, Saldo: $ {Saldo:F2}";
+        }
+        
+
+
+
+
+
+    }
+}
