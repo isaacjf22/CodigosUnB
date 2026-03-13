e8775600a234dcbe135b307dbf6f932e777846f8
Isaac Pereira Ferreira <isaacjf321@gmail.com>
2026-03-13 18:55:18 -0300

    Add exercicios de matrizes C#

---
+++ C#/Matriz/MatrizLeitura/MatrizLeitura/Program.cs
@@ -0,0 +1,42 @@
+﻿//Fazer um programa para ler um número inteiro N e uma matriz de
+//ordem N contendo números inteiros. Em seguida, mostrar a diagonal
+//principal e a quantidade de valores negativos da matriz.
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Qual é a ordem da sua matriz?");
+        int ordemMatriz = int.Parse(Console.ReadLine());
+        Console.WriteLine();
+
+        int[,] matriz = new int[ordemMatriz, ordemMatriz]; //instanciando a matriz 
+
+        string diagonalPrincipal = "";
+        int negativos = 0;
+
+        for(int i = 0; i < matriz.GetLength(0); i++)//percorrendo a linha , 'ponto referencia' 
+        {
+            for(int j=0;j<matriz.GetLength(1); j++)//percorrendo as colunas da linha , 'ponto varredura'
+            {
+                Console.WriteLine($"Digite o valor do elemento da linha {i+1} e coluna {j+1}");
+                matriz[i,j] = int.Parse(Console.ReadLine());
+                if (i == j) //se estiver nessa posição de linha = coluna 
+                {
+                    diagonalPrincipal += matriz[i, j] + ", "; //precisa acumular , para juntar todos os resultados
+                }
+                if(matriz[i, j] < 0) //se for negativo , é contalizado 
+                {
+                    negativos++;
+                }
+            }
+        }
+
+        Console.WriteLine("Diagonal principal: ");
+        Console.WriteLine(diagonalPrincipal);
+        Console.WriteLine($"Quantidade de números negativos: {negativos}");
+
+
+
+    }
+}
---
+++ C#/Matriz/MatrizVizinhos.cs
@@ -0,0 +1,70 @@
+﻿//Fazer um programa para ler dois números inteiros M e N, e depois ler
+//uma matriz de M linhas por N colunas contendo números inteiros,
+//podendo haver repetições. Em seguida, ler um número inteiro X que
+//pertence à matriz. Para cada ocorrência de X, mostrar os valores à
+//esquerda, acima, à direita e abaixo de X, quando houver, conforme
+//exemplo.
+
+using System.ComponentModel;
+using System.Linq.Expressions;
+using System.Security.Cryptography.X509Certificates;
+
+class MatrizVizinhos
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Digite a linha e coluna da matriz a ser analisada: ");
+        string[] v = Console.ReadLine().Split(' ');
+        int mLinha = int.Parse(v[0]);
+        int nColuna = int.Parse(v[1]);
+
+        int[,] matriz = new int[mLinha, nColuna]; //instanciando a matriz 
+
+        //entrada de valores 
+
+
+        Console.WriteLine("Entre os elementos da matriz: ");
+        for (int i = 0; i < matriz.GetLength(0); i++) // linha 
+        {
+            for (int j = 0; j < matriz.GetLength(1); j++)//colunas da linha 
+            {
+                matriz[i, j] = int.Parse(Console.ReadLine());
+            }
+        }
+
+
+        Console.WriteLine("Qual elemento você quer analisar?");
+
+        int numeroEscolhido = int.Parse(Console.ReadLine());
+
+        
+             
+        for (int i = 0; i < matriz.GetLength(0); i++)
+        {
+            for (int j = 0; j < matriz.GetLength(1); j++)
+            {
+                if (matriz[i, j] == numeroEscolhido)//melhor fazer logo aq no principal , e não criar uma classe 
+                {
+                    Console.WriteLine($"Posição {i},{j}");
+                    if (j > 0) //condições para o valor estar dentro da matriz 
+                    {
+                        Console.WriteLine($"Esquerda: {matriz[i,j-1]}");
+                    }
+                    if (i > 0)
+                    {
+                        Console.WriteLine($"Cima: {matriz[i-1,j]}");
+                    }
+                    if (j < nColuna - 1)
+                    {
+                        Console.WriteLine($"Direita: {matriz[i,j+1]}");
+                    }
+                    if (i < mLinha - 1)
+                    {
+                        Console.WriteLine($"Baixo: {matriz[i+1,j]}");
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
